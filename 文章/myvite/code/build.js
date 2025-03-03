d74ac1f0a8556e173e86bf8fa5c34045031a19a1
yaoyh <1423690047@qq.com>
2025-03-03 19:01:00 +0800

    feat: build

--- 文章/myvite/code/build.js
+++ 文章/myvite/code/build.js
@@ -1,6 +1,5 @@
 import path from 'node:path'
 import fs from 'node:fs'
-import fsp from 'node:fs/promises'
 import { rollup } from 'rollup'
 import terser from '@rollup/plugin-terser'
 import html from '@rollup/plugin-html'
@@ -10,59 +9,80 @@ import url from '@rollup/plugin-url'
 import vue from 'rollup-plugin-vue'
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace'
-import progress from 'rollup-plugin-progress'
+import alias from '@rollup/plugin-alias'
 import esbuild from 'rollup-plugin-esbuild'
-
-import colors from 'picocolors'
+import { fileURLToPath, URL } from 'node:url'
 
 const BUILD_HTMLTITLE = 'my App'
-const SOURCE_SCRIPTMODULE = '<script type="module" src="/src/main.ts"></script>'
 
-const clearDir = async (dir) => {
+function clearDir(dir) {
   if (fs.existsSync(dir)) {
     fs.rmSync(dir, { recursive: true })
   }
 }
 
-const createBuild = async (root, config) => {
+function findInputInRoot(root) {
+  const rootPath = path.join(root, 'src')
+
+  for (const file of fs.readdirSync(rootPath)) {
+    if (file.startsWith('main')) {
+      return `/src/${file}`
+    }
+  }
+
+  throw new Error('No entry file found in src directory')
+}
+
+
+async function createBuild(root, config) {
   const { outDir, assetsDir='assets' } = config
-  const isJsx = fs.existsSync(path.join(root, 'src/main.jsx'))
   const template = path.join(root, 'index.html')
-  const input = path.join(root, isJsx ? 'src/main.jsx' : 'src/main.ts')
   const dir = path.join(root, outDir || 'dist')
+  const main = findInputInRoot(root)
+  const input = path.join(root, main)
+
   const inputOptions = {
     input,
     plugins: [
+      // 可以代码中使用 @ 别名
+      alias({
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }),
+      // 解析 .vue 文件为 sfc
       vue(),
+      // 处理css
       styles({
         mode: 'extract',
         minimize: true,
       }),
+      // esbuild，这里用来编译 TypeScript
       esbuild({ target: 'esnext', loader: 'ts' }), // 使用 esbuild 编译 TypeScript
+      // rollup 能够识别和处理 npm 包
       resolve(),
-      // alias({
-      //   entries: [
-      //     { find: 'dayjs', replacement: 'dayjs/esm' }
-      //   ]
-      // }),
+      // 将代码中的 process.env.NODE_ENV 替换为 production
       replace({
         preventAssignment: true,
         'process.env.NODE_ENV': JSON.stringify('production'),
       }),
+      // rollup 能够识别 commonjs 模块
       commonjs(),
+      // 处理图片
       url({
         include: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.gif'],
         limit: 0,
         fileName: `./${assetsDir}/[name]-[hash][extname]`,
       }),
+      // 处理 html 文件。这里为了 html 文件中引入编译新生成的 css 和 js 文件。并删除对开发环境的 css 和 js 的引入
       html({
         title: BUILD_HTMLTITLE,
         template: async ({ files }) => {
           // files 是一个对象，包含了 entrypoints 生成的所有文件
           // 我们需要把他们按顺序引入到 html 模板中
+
+          let code = fs.readFileSync(template).toString()
+          code = code.replace(`<script type="module" src="${main}"></script>`, '')
+
           let fileTags = []
-          let code = (await fsp.readFile(template)).toString()
-          code = code.replace(SOURCE_SCRIPTMODULE, '')
           if (files.css) {
             const cssTags = files.css.map((item) => {
               return '\t' + `<link rel="stylesheet" href="/${item.fileName}">`
@@ -83,8 +103,8 @@ const createBuild = async (root, config) => {
           return code
         },
       }),
-      terser(),
-      progress(),
+      // 压缩 js 文件
+      terser()
     ],
   }
   const outputOptions = {
@@ -100,7 +120,7 @@ const createBuild = async (root, config) => {
   await bundle.write(outputOptions)
   await bundle.close()
   const end = Date.now()
-  console.log(colors.green(`✓ built in ${Math.round(end - start)}ms`))
+  console.log(`✓ built in ${Math.round(end - start)}ms`)
 }
 
 export { createBuild }
